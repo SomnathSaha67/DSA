89316fda33bfa237a219793ae32932d601fe6e5b
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-08-22 23:28:03 +0530

    Time: 0 ms (100%), Space: 44.8 MB (48.73%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -2,13 +2,12 @@ class Solution {
     public int searchInsert(int[] nums, int target) {
         int start=0, end= nums.length-1;
         while(start<=end){
-            int mid= start+(end-start)/2;
-            if (nums[mid] == target) {
-                return mid; 
-            } else if (nums[mid] < target) {
-                start = mid + 1; 
+            if (nums[start+(end-start)/2] == target) {
+                return start+(end-start)/2; 
+            } else if (nums[start+(end-start)/2] < target) {
+                start = start+(end-start)/2 + 1; 
             } else {
-                end = mid - 1;  
+                end = start+(end-start)/2 - 1;  
             }
         }
         return start;

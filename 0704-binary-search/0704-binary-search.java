d2314a12289142f6cc03c60e4b17798317415db5
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-08-22 23:12:23 +0530

    Time: 0 ms (100%), Space: 48.4 MB (50.32%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int start=0, end= nums.length-1;
+        while(start<=end){
+            int mid= start+(end-start)/2;
+            if (target==nums[mid])
+                return mid;
+            if (target<nums[mid])
+                end= mid-1;
+            else
+                start= mid+1;
+        }
+        return -1;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

bdf92e2c2eee8b44dba7fbfaaef2f1cfd52fbb15
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-08-24 17:38:30 +0530

    Added solution

---
+++ 707364-split-array-largest-sum-GFG/707364-split-array-largest-sum.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int splitArray(int[] arr, int k) {
+        // code here
+        int start=0, end=0;
+        for(int num:arr){
+            start= Math.max(start, num);
+            end+= num;
+        }
+        while(start<end){
+            int mid= start+(end-start)/2;
+            int sum=0, pieces=1;
+            for (int num:arr){
+                if (sum+num>mid){
+                    sum= num;
+                    pieces++;
+                }
+                else{
+                    sum+=num;
+                }
+            }
+            if (pieces>k){
+                start= mid+1;
+            }
+            else{
+                end= mid;
+            }
+        }
+        return start;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

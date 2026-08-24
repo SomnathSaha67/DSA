78cc271f2d3ecd6cdb412fa9912beb1b7b33b83c
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-08-24 18:34:18 +0530

    Added solution

---
+++ 703459-first-and-last-in-sorted-GFG/703459-first-and-last-in-sorted.java
@@ -0,0 +1,35 @@
+class Solution {
+    ArrayList<Integer> find(int arr[], int x) {
+        // code here
+        ArrayList<Integer> ans= new ArrayList<>();
+        ans.add(0, findIndex(arr, x, true));
+        ans.add(1, findIndex(arr, x, false));
+        return ans;
+        
+    }
+    public int findIndex(int[]arr, int target, boolean findFirstIndex){
+        int start=0, end= arr.length-1, ans= -1;
+        while(start<=end){
+            int mid= start+(end-start)/2;
+            if (target<arr[mid]){
+                end= mid-1;
+            }else if(target>arr[mid]){
+                start= mid+1;
+            }else{
+                ans= mid;
+                if (findFirstIndex){
+                    end= mid-1;
+                }else{
+                    start= mid+1;
+                }
+            }
+        }
+        return ans;
+    }
+    
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

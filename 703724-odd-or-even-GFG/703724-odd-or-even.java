a28c5e73313779f2d20eda0b62879499752bbafc
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-08-24 17:47:22 +0530

    Added solution

---
+++ 703724-odd-or-even-GFG/703724-odd-or-even.java
@@ -0,0 +1,10 @@
+class Solution {
+    static boolean isEven(int n) {
+        // code here
+        return n%2==0;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

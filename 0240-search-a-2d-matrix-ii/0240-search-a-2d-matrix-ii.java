4d9d1c18d8725bcc1607436cc9986bca0c4b423d
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-08-25 19:24:37 +0530

    Time: 4 ms (22.2%), Space: 48.2 MB (45.59%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int ele=-1;
+        for (int[]row:matrix){
+            ele= binarySearch(row, target);
+            if(ele!=-1)
+                return true;
+        }
+        return false;
+    }
+    public int binarySearch(int[]arr, int target){
+        int start=0, end= arr.length-1;
+        while(start<=end){
+            int mid= start+(end-start)/2;
+            if (arr[mid]<target)
+                start= mid+1;
+            else if(arr[mid]>target)
+                end= mid-1;
+            else
+                return mid;
+        }
+        return -1;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

28b4d98bde72f11d12d2004834e60baa54b25b15
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-08-26 12:47:23 +0530

    Added solution

---
+++ 700143-bubble-sort-GFG/700143-bubble-sort.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void bubbleSort(int[] arr) {
+        // code here\
+        boolean swapped;
+        for (int i=0; i<arr.length-1; i++){
+            swapped=false;
+            for (int j=1; j<arr.length-i; j++){
+                if (arr[j]<arr[j-1]){
+                    int temp= arr[j];
+                    arr[j]= arr[j-1];
+                    arr[j-1]= temp;
+                    swapped= true;
+                }
+            }
+            if (!swapped)
+                break;
+        }
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

94954cc852c7bf6f1ca8e662c495395fb7818174
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-08-26 16:37:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Selection Sort/selection-sort.java
@@ -0,0 +1,23 @@
+class Solution {
+    void selectionSort(int[] arr) {
+        // code here
+        for (int i=0; i<arr.length-1; i++){
+            int lastIndex= arr.length-i-1;
+            int maxIndex= getMax(arr, 0, lastIndex);
+            swap(arr, maxIndex, lastIndex);
+        }
+    }
+    int getMax(int[]arr, int start, int end){
+        int max= start;
+        for (int i= start; i<=end; i++){
+            if(arr[max]<arr[i])
+                max= i;
+        }
+        return max;
+    }
+    void swap(int[]arr, int maxIndex, int lastIndex){
+        int temp= arr[maxIndex];
+        arr[maxIndex]= arr[lastIndex];
+        arr[lastIndex]= temp;
+    }
+}

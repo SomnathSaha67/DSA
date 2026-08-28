dd5a1948ea90a3a968de746593f3c1066d42e027
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-08-28 20:22:25 +0530

    Added solution - Time: O(1), Space: O(1)

---
+++ 700947-multiplication-table-GFG/700947-multiplication-table.py
@@ -0,0 +1,9 @@
+n = int(input())
+
+# code here
+for i in range(1, 11):
+    print(n*i, end=" ")
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

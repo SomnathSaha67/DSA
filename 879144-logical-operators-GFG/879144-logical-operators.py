c91495d5a45430f52c839954241189576f0b4936
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-08-28 14:44:54 +0530

    Added solution - Time: O(1), Space: O(1)

---
+++ 879144-logical-operators-GFG/879144-logical-operators.py
@@ -0,0 +1,14 @@
+a=int(input())
+b=int(input())
+#code here
+p = a and b
+#Do a or b below
+q = a or b
+#Do not a below
+r = not a
+#The code below prints the output. Don't change it!
+print(p,q,r)
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

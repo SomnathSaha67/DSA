62195b7dc3f7b9009414143d66b6aec20c2177de
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-08-28 20:24:05 +0530

    Added solution - Time: O(n), Space: O(1)

---
+++ 704473-even-positioned-characters-GFG/704473-even-positioned-characters.py
@@ -0,0 +1,8 @@
+def utility(s):
+    #code here
+    for i in range(0,len(s),2):
+        print(s[i],end="")
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

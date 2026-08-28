39f96b9be0f7fbb4658138d3f48199769bec02f9
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-08-28 20:31:45 +0530

    Added solution - Time: O(sqrt(x)), Space: O(1)

---
+++ 700951-jumping-through-while-python-GFG/700951-jumping-through-while-python.py
@@ -0,0 +1,16 @@
+def printIncreasingPower(x):
+    #code here
+    # Loop to jump in powers of 2
+    i=1
+    while(i<x):
+        #code here
+        if i*i>x:
+            break
+        else:
+            print (i*i, end = " ")
+            i+=1
+        #code here
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

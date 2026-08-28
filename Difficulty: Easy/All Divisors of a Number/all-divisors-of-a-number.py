bb84c8878abb6e052071d7f0a5556031ba43d627
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-08-28 21:47:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/All Divisors of a Number/all-divisors-of-a-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def getDivisors(self, n):
+        # code here
+        divisors = []
+        for i in range(1, int(n**0.5) + 1):
+            if n % i == 0:
+                divisors.append(i)
+                if i != n // i: 
+                    divisors.append(n // i)
+        return sorted(divisors)

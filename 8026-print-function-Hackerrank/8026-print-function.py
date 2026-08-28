838079720643380f8459c4ea18985b954099192e
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-08-28 20:20:43 +0530

    Force committed solution - Time: O(n log n), Space: O(n log n)

---
+++ 8026-print-function-Hackerrank/8026-print-function.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    n = int(input())
+    new=""
+    for i in range(1, n+1):
+        new+=str(i)
+    print(new)
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

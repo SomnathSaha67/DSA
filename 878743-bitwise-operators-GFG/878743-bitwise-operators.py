830c44f388baf6932ad9f64a6adfba82f495cb60
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-08-28 14:46:25 +0530

    Added solution - Time: O(1), Space: O(1)

---
+++ 878743-bitwise-operators-GFG/878743-bitwise-operators.py
@@ -0,0 +1,14 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+# code here
+d = a ^ a
+e = c ^ b
+f = a & b
+g = ~ e
+print(d, e, f, g)
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

df4de50b9d6acb56dded8fb562b56810bfabe913
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-08-28 12:53:43 +0530

    Added solution - Time: O(?), Space: O(?)

---
+++ 879142-int-str-GFG/879142-int-str.py
@@ -0,0 +1,9 @@
+a = input()
+b = input()
+c = input()
+# Write your code below that prints a a times and b b times, seperated by c
+print(a*int(a), b*int(b), sep= c)
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

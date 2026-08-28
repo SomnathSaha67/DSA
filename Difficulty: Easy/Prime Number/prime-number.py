8355ea6b003f103291ad6ca7124ffdf2b9e61664
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-08-28 21:49:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Prime Number/prime-number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPrime(self, n):
+        # code here
+        if n<=2:
+            return False
+        else:
+            for i in range(2, n//2):
+                if(n%i==0):
+                    return False
+            return True
+        

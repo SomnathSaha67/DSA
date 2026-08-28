d6f633ff80eaa7fa844ab0a86ec0ad315e5d735d
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-08-28 21:12:29 +0530

    Added solution - Time: O(n), Space: O(n)

---
+++ 705361-right-angle-triangle-pattern-GFG/705361-right-angle-triangle-pattern.py
@@ -0,0 +1,9 @@
+n = int(input())
+
+# code here
+for i in range(1, n+1):
+    print("* "*i)
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

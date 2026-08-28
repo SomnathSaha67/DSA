2b7644214dce582071150f51645df3662d7c3e1e
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-08-28 12:47:47 +0530

    Added solution - Time: O(1), Space: O(1)

---
+++ 700796-learn-to-comment-GFG/700796-learn-to-comment.py
@@ -0,0 +1,17 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+# Prints a
+print(a)
+
+# Prints b
+#print(b)
+
+# Prints c
+print(c)
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

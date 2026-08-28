b57bd3d7b181a973286586d852f593952a28d4e4
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-08-28 21:03:28 +0530

    Added solution - Time: O(1), Space: O(1)

---
+++ 703926-greatest-of-three-numbers-GFG/703926-greatest-of-three-numbers.py
@@ -0,0 +1,12 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+# code here
+if(b<a>c): print(a)
+elif(a<b>c): print(b)
+else: print(c)
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

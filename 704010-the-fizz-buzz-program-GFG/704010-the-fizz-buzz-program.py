8e989afb55f9004d287e644802abb569c77f1c45
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-08-28 21:00:59 +0530

    Added solution - Time: O(1), Space: O(1)

---
+++ 704010-the-fizz-buzz-program-GFG/704010-the-fizz-buzz-program.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+# code here
+if(n%3==0 and n%5==0): print("FizzBuzz")
+elif(n%3==0): print("Fizz")
+elif(n%5==0): print("Buzz")
+else: print(n)
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

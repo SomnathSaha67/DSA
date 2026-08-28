9bbaee5b3b7ffc9b38e3749a363465625bf31319
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-08-28 14:50:06 +0530

    Added solution - Time: O(1), Space: O(1)

---
+++ 704102-swap-two-numbers-GFG/704102-swap-two-numbers.py
@@ -0,0 +1,11 @@
+class Solution:
+
+    def swap(self, a, b):
+        #code here
+        a,b= b,a
+        print(a, b)
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

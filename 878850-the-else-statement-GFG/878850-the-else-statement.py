89b9b5edc46801a13036ed7b7669b61b109a2da8
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-08-28 20:57:45 +0530

    Added solution - Time: O(1), Space: O(1)

---
+++ 878850-the-else-statement-GFG/878850-the-else-statement.py
@@ -0,0 +1,11 @@
+a = int(input())
+
+# code here
+if(a<=100):
+    print("Small")
+else:
+    print("Big")
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

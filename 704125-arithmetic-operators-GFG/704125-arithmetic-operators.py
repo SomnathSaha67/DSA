accde51d5896c2eb4f2d2af935b86b86417c0bb1
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-08-28 14:43:28 +0530

    Added solution - Time: O(1), Space: O(1)

---
+++ 704125-arithmetic-operators-GFG/704125-arithmetic-operators.py
@@ -0,0 +1,10 @@
+x = int(input())
+y = int(input())
+
+# code here
+p= x+y; q= x-y; r= x*y; s= float(x)/float(y); t= x//y; u= x%y
+print(p, q, r, f"{s:.3f}", t, u)
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

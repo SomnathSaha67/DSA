6aec833c44d980d299831148bd811bdb94f6f0bf
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-08-28 14:41:21 +0530

    Added solution - Time: O(N), Space: O(N)

---
+++ 880511-taking-input-GFG/880511-taking-input.py
@@ -0,0 +1,17 @@
+import math
+
+# s to store string
+# n to store integer
+# f to store float
+# ff  # To Store floor of float variable f
+
+# code here
+s= input(); n= int(input()); ff= int(float(input()))
+print(s)
+print(n)
+print(ff)
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

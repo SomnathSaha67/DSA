9ab440b6c7d108ad33de2f8b551e4e27d103060c
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-08-28 21:06:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Leap Year/leap-year.py
@@ -0,0 +1,8 @@
+class Solution:
+    def checkYear (self, n):
+        # code here
+        if((n%100!=0 and n%4==0) or n%400==0):
+            return True
+        else:
+            return False
+        

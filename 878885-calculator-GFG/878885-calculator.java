786ef9ab5653e80a43fd0ec564fe62610c9ecd9d
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-08-28 21:08:11 +0530

    Added solution - Time: O(1), Space: O(1)

---
+++ 878885-calculator-GFG/878885-calculator.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void calculate(int a, int b, int optr) {
+        // code here
+        if (optr==1){
+            System.out.print(a+b);
+        }else if (optr==2){
+            System.out.print(a-b);
+        }else if (optr==3){
+            System.out.print(a*b);
+        }else{
+            System.out.print("Invalid Input");
+        }
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

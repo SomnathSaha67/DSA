58afbbec0e25946972df9bb64fde5ada14804806
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-08-28 21:02:19 +0530

    Added solution - Time: O(1), Space: O(1)

---
+++ 878871-even-odd-game-GFG/878871-even-odd-game.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+# code here
+if(n%2==0):
+    print("Friend")
+else:
+    print("You")
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

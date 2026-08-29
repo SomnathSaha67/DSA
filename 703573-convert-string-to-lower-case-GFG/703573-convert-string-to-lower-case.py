e5b6a15565deba18d95bf018cb8d3d66310f0994
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-08-29 20:14:02 +0530

    Added solution - Time: O(n), Space: O(n)

---
+++ 703573-convert-string-to-lower-case-GFG/703573-convert-string-to-lower-case.py
@@ -0,0 +1,9 @@
+class Solution:
+    def toLower (self, s: str) -> str:
+        # code here 
+        return s.lower()
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

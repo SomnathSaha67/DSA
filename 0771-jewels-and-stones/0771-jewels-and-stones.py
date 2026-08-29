c71be99c8295967e181369eb0c1c859e15855077
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-08-29 20:53:05 +0530

    Time: 3 ms (14.88%), Space: 12.4 MB (16.38%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def numJewelsInStones(self, jewels, stones):
+        """
+        :type jewels: str
+        :type stones: str
+        :rtype: int
+        """
+        c=0
+        for char in jewels:
+            for mar in stones:
+                if char==mar:
+                    c+=1
+        return c
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

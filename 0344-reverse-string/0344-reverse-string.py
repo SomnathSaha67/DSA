9c29360ad6b4f9e721c85808bdf3b8fc9abd8677
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-08-29 20:42:41 +0530

    Time: 0 ms (100%), Space: 19.9 MB (43.19%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        return s.reverse()
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

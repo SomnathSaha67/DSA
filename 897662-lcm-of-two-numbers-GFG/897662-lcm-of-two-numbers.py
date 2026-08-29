dbfa934051e16a5fa6f77b32e07d118fd824de60
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-08-29 10:50:56 +0530

    Added solution - Time: O(log(min(a, b))), Space: O(1)

---
+++ 897662-lcm-of-two-numbers-GFG/897662-lcm-of-two-numbers.py
@@ -0,0 +1,10 @@
+import math
+class Solution:
+       
+    def lcm(self, a, b):
+        # code here
+        return math.lcm(a,b)
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

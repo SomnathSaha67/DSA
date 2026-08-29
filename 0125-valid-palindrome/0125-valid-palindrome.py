b77ec000cf6d514154fe0146823703cca2972c6c
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-08-29 20:37:09 +0530

    Time: 15 ms (71.79%), Space: 14.1 MB (21.45%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -4,22 +4,8 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        newstr=""
-        orgstr=""
-        if s.isspace():
-            return True
-        else:
-            for c in s:
-                if c.isdigit():
-                    newstr+=c
-                elif c.isalpha():
-                    newstr+=c
-            for c in s:
-                if c.isdigit():
-                    orgstr+=c
-                elif c.isalpha():
-                    orgstr+=c
-            return orgstr.lower()==newstr[::-1].lower()
+        filter= ''.join(c.lower() for c in s if c.isalnum())
+        return filter==filter[::-1]
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4

31de96903cdc64c35670e02e0e12263161fae16c
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-08-30 18:12:26 +0530

    Added solution - Time: O(N), Space: O(N)

---
+++ 878235-count-distinct-in-an-array-GFG/878235-count-distinct-in-an-array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def countDistinct(self, arr):
+        # code here
+        return len(set(arr))
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

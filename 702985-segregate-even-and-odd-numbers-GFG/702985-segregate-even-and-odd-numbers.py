4b25d4ca4ea8dcbcad02d04af0993d94d2e3cca7
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-08-30 17:40:26 +0530

    Added solution - Time: O(n log n), Space: O(n)

---
+++ 702985-segregate-even-and-odd-numbers-GFG/702985-segregate-even-and-odd-numbers.py
@@ -0,0 +1,13 @@
+class Solution:
+
+	def segregateEvenOdd(self,arr):
+		# code here
+		evens= [x for x in arr if x%2==0]
+		odds= [x for x in arr if x%2!=0]
+		evens.sort()
+		odds.sort()
+		arr[:]= evens+odds
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

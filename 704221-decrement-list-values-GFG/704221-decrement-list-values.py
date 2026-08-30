2b46180e74e96fed0db798aef022d411abc31265
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-08-30 17:12:02 +0530

    Added solution - Time: O(n), Space: O(n)

---
+++ 704221-decrement-list-values-GFG/704221-decrement-list-values.py
@@ -0,0 +1,8 @@
+def decrementList(arr):
+    #code here
+    ar=[ele-1 for ele in arr]
+    return ar
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

010a270c007fe4ef73f729568e0e2f029aea584e
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-08-30 17:17:39 +0530

    Added solution - Time: O(n), Space: O(n)

---
+++ 704533-less-than-GFG/704533-less-than.py
@@ -0,0 +1,10 @@
+class Solution:
+
+    def lessThan(self, arr, k):
+        #code here
+        lst= [e for e in arr if e<k]
+        return lst
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

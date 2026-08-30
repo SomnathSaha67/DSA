f6c9da4d4d89ffd430afbc94508cd9e05213fd2f
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-08-30 12:17:36 +0530

    Force committed solution

---
+++ 7885-power-mod-power-Hackerrank/7885-power-mod-power.py
@@ -0,0 +1,11 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a= int(input())
+b= int(input())
+m= int(input())
+print(pow(a, b))
+print(pow(a, b, m))
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

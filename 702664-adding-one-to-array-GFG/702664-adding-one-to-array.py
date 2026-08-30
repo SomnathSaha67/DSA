3f980bfccb69778f63d5586826cbefc089309fa5
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-08-30 18:42:43 +0530

    Added solution - Time: O(N), Space: O(1)

---
+++ 702664-adding-one-to-array-GFG/702664-adding-one-to-array.py
@@ -0,0 +1,17 @@
+# function for adding one to number 
+class Solution:
+    # Function to add one to a number represented as an array
+    def addOne(self, arr):
+        # code here
+        carry=1
+        for i in range(len(arr)-1, -1, -1):
+            total= arr[i]+carry
+            arr[i]= total%10
+            carry= total//10
+        if carry:
+            arr.insert(0, carry)
+        return arr
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

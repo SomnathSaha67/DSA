685cc34f822a5702d8b9e967e93ec472e6c9234a
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-08-30 17:16:18 +0530

    Added solution - Time: O(1), Space: O(1)

---
+++ 704222-append-to-list-GFG/704222-append-to-list.py
@@ -0,0 +1,8 @@
+def appendToList(a, b, c):
+    # code here
+    lst=[e for e in (a, b, c)]
+    return lst
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

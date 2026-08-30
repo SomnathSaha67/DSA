f7255c94b1fa0c3778edc758ef58e01237e0e848
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-08-30 12:28:22 +0530

    Force committed solution

---
+++ 7882-integers-come-in-all-sizes-Hackerrank/7882-integers-come-in-all-sizes.py
@@ -0,0 +1,12 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+result= pow(a,b)+pow(c,d)
+print(result)
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

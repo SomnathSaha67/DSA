36f8dc9771b06640daf740eb360deead4f8507de
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-08-30 12:11:55 +0530

    Force committed solution

---
+++ 12523-triangle-quest-2-Hackerrank/12523-triangle-quest-2.py
@@ -0,0 +1,11 @@
+
+
+
+
+for i in range(1,int(input())+1): #More than 2 lines will result in 0 score. Do not leave a blank line also
+    print(((10**i)//9)**2) 
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

14e8b10d6dd1971a88923169038f16eb622240df
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-08-30 12:47:16 +0530

    Force committed solution - Time: O(n), Space: O(n)

---
+++ 14254-capitalize-Hackerrank/14254-capitalize.py
@@ -0,0 +1,10 @@
+
+
+# Complete the solve function below.
+def solve(s):
+    return ' '.join([word.capitalize() for word in s.split(' ')])
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

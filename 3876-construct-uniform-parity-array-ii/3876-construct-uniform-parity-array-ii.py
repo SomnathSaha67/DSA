1829f6c583697917ca412ab8547e1f24ffb64402
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-09-03 17:59:48 +0530

    Time: 195 ms (7.69%), Space: 21.7 MB (100%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.py
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.py
@@ -1,20 +1,29 @@
 class Solution(object):
     def uniformArray(self, nums1):
-        """
-        :type nums1: List[int]
-        :rtype: bool
-        """
-        odds= [x for x in nums1 if x%2==1]
-        evens= [x for x in nums1 if x%2==0]
-        if not odds or not evens:
+        odds, evens = 0, 0
+        min_odd, min_even = float('inf'), float('inf')
+        for x in nums1:
+            if x % 2:
+                odds += 1
+                min_odd = min(min_odd, x)
+            else:
+                evens += 1
+                min_even = min(min_even, x)
+        if odds == 0 or evens == 0:
             return True
-        min_odd = min(odds)
-        if all(((x - min_odd) % 2 == 1 and x - min_odd >= 1) or (x % 2 == 1) for x in nums1):
+        can_all_odd = True
+        for x in nums1:
+            if x % 2 == 0:  
+                if (x - min_odd) < 1 or (x - min_odd) % 2 == 0:
+                    can_all_odd = False
+                    break
+        if can_all_odd:
             return True
-        min_even = min(evens)
-        if all(((x - min_even) % 2 == 0 and x - min_even >= 1) or (x % 2 == 0) for x in nums1):
-            return True
-        return False
+        for x in nums1:
+            if x % 2 == 1: 
+                if (x - min_even) < 1 or (x - min_even) % 2 == 1:
+                    return False
+        return True
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4

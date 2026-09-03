17307925ed5f9e368e8010da9df771e323cde45e
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-09-03 17:53:10 +0530

    Time: 0 ms (100%), Space: 12.4 MB (64.56%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def uniformArray(self, nums1):
+        """
+        :type nums1: List[int]
+        :rtype: bool
+        """
+        return True
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

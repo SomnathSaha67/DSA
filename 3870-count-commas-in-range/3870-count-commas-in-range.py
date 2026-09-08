0b65ac8776f79cc94171a6f6ecf588f1da396172
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-09-08 23:22:38 +0530

    Time: 0 ms (100%), Space: 12.4 MB (32.04%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def countCommas(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        return max(0, n-999)
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

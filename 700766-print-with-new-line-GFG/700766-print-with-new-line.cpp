40c493258ddf567a725442adad7640de71ab83c1
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-09-11 17:10:06 +0530

    Added solution - Time: O(1), Space: O(1)

---
+++ 700766-print-with-new-line-GFG/700766-print-with-new-line.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // code here
+    cout<<"Geeks"<<endl;
+    cout<<"for"<<endl;
+    cout<<"Geeks"<<endl;
+
+    return 0;
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

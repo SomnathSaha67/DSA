b29f3bc87062acc78cbf8c5ec664e6dc6d273fcd
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-09-11 17:01:07 +0530

    Added solution - Time: O(1), Space: O(1)

---
+++ 880511-taking-input-GFG/880511-taking-input.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s;
+    int n;
+    float f; // To Store floor of float variable f
+
+    // code here
+    getline(cin, s);
+    cin>>n>>f;
+    
+    cout << s << endl;
+    cout << n << endl;
+    cout << (int)f << endl;
+    return 0;
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

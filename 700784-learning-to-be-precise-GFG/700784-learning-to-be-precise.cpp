96c6d0c7e860b2aa98b595ce13e85c01c1ff0373
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-09-11 17:13:16 +0530

    Added solution - Time: O(1), Space: O(1)

---
+++ 700784-learning-to-be-precise-GFG/700784-learning-to-be-precise.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    void precise(float a, float b) {
+        // code here
+        cout<<fixed<<setprecision(3)<<a/b<<endl;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

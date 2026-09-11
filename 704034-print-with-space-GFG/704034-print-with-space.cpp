d66b2f536ca3266160a826b12d3f30abfd19af3f
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-09-11 17:07:37 +0530

    Added solution - Time: O(1), Space: O(1)

---
+++ 704034-print-with-space-GFG/704034-print-with-space.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string a, b;
+    cin >> a >> b;
+
+    // code here
+    cout<<a<<" "<<b<<endl;
+
+    return 0;
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

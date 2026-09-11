244a9f03cbb57b56d68cdc0c38b537bb63d55f34
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-09-11 16:34:40 +0530

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 12103-input-and-output-Hackerrank/12103-input-and-output.cpp
@@ -0,0 +1,24 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    
+    int a, b, c;
+    
+    cin>>a>>b>>c;
+    
+    cout<<a+b+c;
+    
+    return 0;
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

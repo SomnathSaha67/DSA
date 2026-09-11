feeeadd291c54f7225d3dcd9b42b09287ce7eb94
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-09-11 17:10:52 +0530

    Added solution - Time: O(1), Space: O(1)

---
+++ 704101-print-with-separator-GFG/704101-print-with-separator.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    string a, b, separator;
+    cin >> a >> b >> separator;
+
+    // code here
+    cout<<a<<separator<<b;
+
+    return 0;
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

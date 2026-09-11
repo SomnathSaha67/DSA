60bcd76472464e15dae2fa32da6cf927c651a281
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-09-11 17:22:03 +0530

    Added solution - Time: O(1), Space: O(1)

---
+++ 706372-compute-ab-GFG/706372-compute-ab.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    double compute(int a, int b) {
+        // code here
+        return float(a)/float(b);
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

c8fe1aba622c9717e8945069c807752295c1ef9d
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-09-12 16:31:04 +0530

    Time: 23 ms (47.48%), Space: 13.6 MB (20.32%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        return tuple(sorted(s))==tuple(sorted(t))
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

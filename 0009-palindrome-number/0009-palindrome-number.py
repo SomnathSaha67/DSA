4b4bb934a6dcadf766417536cf63b0788b93a170
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-09-12 14:32:50 +0530

    Time: 9 ms (64.46%), Space: 12.4 MB (56.03%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        return str(x)==str(x)[::-1]
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna

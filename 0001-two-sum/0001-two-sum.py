e056387eabd7d19e5868c82ec99f29cb6e76215f
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2026-09-12 16:24:10 +0530

    Time: 2 ms (57.36%), Space: 12.9 MB (95.26%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        lookup= {}
+
+        for i, num in enumerate(nums):
+
+            comp= target-num
+
+            if comp in lookup: return [lookup[comp], i]
+            else: lookup[num]= i
+        return [-1, -1]
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
